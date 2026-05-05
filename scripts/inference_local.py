dcd43814a2197792fbd6b752671a27928baf60ff
alexgilevich <2484265+alexgilevich@users.noreply.github.com>
2026-05-05 09:02:36 +0200

    Moved local training/inference scripts out of the src folder because they have nothing to do with the source code itself

--- scripts/inference_local.py
+++ scripts/inference_local.py
@@ -1,4 +1,8 @@
 import os
+import sys
+
+script_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../src/')
+sys.path.append(script_dir)
 
 import pandas as pd
 from pyspark.sql.types import * 
@@ -10,18 +14,18 @@ from logging_factory import get_logger
 import numpy as np
 from tabulate import tabulate
 
-DEFAULT_MODEL_LOCAL_SAVE_PATH = "../../model_artifacts/mlflow_models/"
+DEFAULT_MODEL_LOCAL_SAVE_PATH = "../model_artifacts/__local__/"
+DEFAULT_DATA_PATH = "../data/"
 
 logger = get_logger(__name__)
 
-DEFAULT_MLFLOW_MODEL_NAME = "lapelicula-movie-recommender-model-18-feb"
 DEFAULT_NUM_MODEL_LAYER_OUTPUTS = 256
 DEFAULT_NUM_EPOCHS = 2
 
 inference_params = { "limit": 3000 }
 
 schema = {"movie_id": "int32", "title": "object", "genres": "object", "year": "int32", "rating_count": "float64", "rating_avg": "float64", "genre_partition0": "int64", "genre_partition1": "int64", "Action": "int64", "Adventure": "int64", "Animation": "int64", "Comedy": "int64", "Crime": "int64", "Documentary": "int64", "Drama": "int64", "Fantasy": "int64", "Film-Noir": "int64", "Horror": "int64", "Kids": "int64", "Musical": "int64", "Mystery": "int64", "Romance": "int64", "Sci-Fi": "int64", "Thriller": "int64", "War": "int64", "Western": "int64"}
-all_movies_pdf = pd.read_csv("all_movies.csv", dtype=schema)
+all_movies_pdf = pd.read_csv(os.path.join(DEFAULT_DATA_PATH, "all_movies.csv"), dtype=schema)
 all_movies_data = all_movies_pdf.drop(columns=['row_id', 'title', 'genres', 'year', 'rating_count', 'rating_avg', 'genre_partition0', 'genre_partition1'], errors="ignore").to_numpy()
 
 examples = [
@@ -36,7 +40,8 @@ examples = [
 ]
 
 
-model_save_path = "model_binary_v10.keras"
+model_file_name = "model.keras"
+model_save_path = os.path.abspath(os.path.join(DEFAULT_MODEL_LOCAL_SAVE_PATH, model_file_name))
 
 model = Model(num_outputs = DEFAULT_NUM_MODEL_LAYER_OUTPUTS, num_epochs=DEFAULT_NUM_EPOCHS, model_save_path=model_save_path)
 
@@ -51,7 +56,6 @@ for i, example in enumerate(examples):
         "movies": all_movies_data
     }])
     predictions = model.predict(input, inference_params)
-    # #logger.info("loaded: %s", predictions)
     recommendations = [(int(movie_id), float(rating)) for movie_id, rating in predictions[0]]
     recommendations_pdf = pd.DataFrame(recommendations, columns=['movie_id', 'rating'])
 
@@ -59,7 +63,7 @@ for i, example in enumerate(examples):
     print(tabulate(recommendations_pdf.iloc[:200], headers = 'keys', tablefmt = 'psql', maxcolwidths=50))
     
     print("\n\n\n\n")
-#logger.info("Model max output difference is: %f", np.max(np.abs(np.array(orig)[:, 1:] - np.array(loaded)[:, 1:])))
+
 
     
 
--- scripts/training_local.py
+++ scripts/training_local.py
@@ -1,14 +1,19 @@
+import sys
 import os
+
+script_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../src/')
+sys.path.append(script_dir)
+
 import pandas as pd
 from pyspark.sql.types import * 
-import _includes
 from model import Model
 from pyspark.sql import functions as F
 from logging_factory import get_logger
 import numpy as np
 import tensorflow as tf
 
-DEFAULT_MODEL_LOCAL_SAVE_PATH = "../../model_artifacts/mlflow_models/"
+DEFAULT_MODEL_LOCAL_SAVE_PATH = "../model_artifacts/__local__/"
+DEFAULT_DATA_PATH = "../data/"
 
 logger = get_logger(__name__)
 
@@ -16,20 +21,19 @@ DEFAULT_NUM_MODEL_LAYER_OUTPUTS = 256
 DEFAULT_NUM_EPOCHS = 30
 
 
-
-
 movie_train_data_schema = {"row_id":"int64","movie_id":"int32","title":"object","Action":"int64","Adventure":"int64","Animation":"int64","Comedy":"int64","Crime":"int64","Documentary":"int64","Drama":"int64","Fantasy":"int64","Film-Noir":"int64","Horror":"int64","Kids":"int64","Musical":"int64","Mystery":"int64","Romance":"int64","Sci-Fi":"int64","Thriller":"int64","War":"int64","Western":"int64"}
 user_train_data_schema = {"row_id":"int64","user_id":"int64","Action":"float64","Adventure":"float64","Animation":"float64","Comedy":"float64","Crime":"float64","Documentary":"float64","Drama":"float64","Fantasy":"float64","Film-Noir":"float64","Horror":"float64","Kids":"float64","Musical":"float64","Mystery":"float64","Romance":"float64","Sci-Fi":"float64","Thriller":"float64","War":"float64","Western":"float64"}
 y_schema = {"row_id":"int64","rating":"float64"}
-#all_movies_schema = {"movie_id": "int32", "title": "object", "year": "float64", "genres": "object", "rating_count": "int64", "rating_avg": "float64", "genre_partition0": "int64", "genre_partition1": "int64", "Action":"int64","Adventure":"int64","Animation":"int64","Comedy":"int64","Crime":"int64","Documentary":"int64","Drama":"int64","Fantasy":"int64","Film-Noir":"int64","Horror":"int64","Kids":"int64","Musical":"int64","Mystery":"int64","Romance":"int64","Sci-Fi":"int64","Thriller":"int64","War":"int64","Western":"int64"}
 all_movies_schema = {"movie_id": "int32", "title": "object", "genres": "object", "year": "int32", "rating_count": "float64", "rating_avg": "float64", "genre_partition0": "int64", "genre_partition1": "int64", "Action": "int64", "Adventure": "int64", "Animation": "int64", "Comedy": "int64", "Crime": "int64", "Documentary": "int64", "Drama": "int64", "Fantasy": "int64", "Film-Noir": "int64", "Horror": "int64", "Kids": "int64", "Musical": "int64", "Mystery": "int64", "Romance": "int64", "Sci-Fi": "int64", "Thriller": "int64", "War": "int64", "Western": "int64"}
 
-all_movies_pdf = pd.read_csv("all_movies.csv", dtype=all_movies_schema)
-movie_train_data_pdf = pd.read_csv("train_data_movies.csv", dtype=movie_train_data_schema)
-user_train_data_pdf = pd.read_csv("train_data_users.csv", dtype=user_train_data_schema)
-y_pdf = pd.read_csv("train_data_y.csv", dtype=y_schema)
+all_movies_pdf = pd.read_csv(os.path.join(DEFAULT_DATA_PATH, "all_movies.csv"), dtype=all_movies_schema)
+movie_train_data_pdf = pd.read_csv(os.path.join(DEFAULT_DATA_PATH, "train_data_movies.csv"), dtype=movie_train_data_schema)
+user_train_data_pdf = pd.read_csv(os.path.join(DEFAULT_DATA_PATH, "train_data_users.csv"), dtype=user_train_data_schema)
+y_pdf = pd.read_csv(os.path.join(DEFAULT_DATA_PATH, "train_data_y.csv"), dtype=y_schema)
 
-model_save_path = "model.keras"
+model_file_name = "model.keras"
+model_save_path = os.path.abspath(os.path.join(DEFAULT_MODEL_LOCAL_SAVE_PATH, model_file_name))
+
 model = Model(num_outputs = DEFAULT_NUM_MODEL_LAYER_OUTPUTS, num_epochs=DEFAULT_NUM_EPOCHS, model_save_path=model_save_path)
 
 
